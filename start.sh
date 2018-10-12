b232a276be61d38139601d5445c93eb7cc1f53d7
Sanyam101 <chawla.sanyam@yahoo.in>
2018-10-12 10:17:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# adding new line
-# check for updates
+# adding commnets for checking hadoop update
 export JENKINS_HOME=/ddsw/jenkins/home
 echo "JENKINS_HOME Set to : " $JENKINS_HOME
 
